32515b2257763d26995ae49230dd37cd6eabf5ea
kyeongjeong <ariel0918@kw.ac.kr>
2024-10-06 15:29:58 +0900

    07-1: 37, 38번 문제

--- src/greedy/greedyAlgorithm/MeetingRoomAllocation.java
+++ src/greedy/greedyAlgorithm/MeetingRoomAllocation.java
@@ -3,36 +3,36 @@ import java.util.*;
 import java.io.*;
 
 public class MeetingRoomAllocation {
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-
-        int[][] A = new int[N][2];
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            A[i][0] = Integer.parseInt(st.nextToken()); // 시작 시간
-            A[i][1] = Integer.parseInt(st.nextToken()); // 종료 시간
-        }
-
-        Arrays.sort(A, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] S, int[] E) {
-                if(S[1] == E[1]) {
-                    return S[0] - E[0];  // 종료 시간이 같으면 시작 시간으로 정렬
-                }
-                return S[1] - E[1];  // 종료 시간 기준 정렬
-            }
-        });
-
-        int count = 0;
-        int end = 0;
-        for(int i = 0; i < N; i++) {
-            if(A[i][0] >= end) {  // 시작 시간이 이전 종료 시간 이후면 회의실 사용 가능
-                end = A[i][1];
-                ++count;
-            }
-        }
-        System.out.println(count);
-    }
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(bf.readLine());
+//
+//        int[][] A = new int[N][2];
+//        for(int i = 0; i < N; i++) {
+//            StringTokenizer st = new StringTokenizer(bf.readLine());
+//            A[i][0] = Integer.parseInt(st.nextToken()); // 시작 시간
+//            A[i][1] = Integer.parseInt(st.nextToken()); // 종료 시간
+//        }
+//
+//        Arrays.sort(A, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] S, int[] E) {
+//                if(S[1] == E[1]) {
+//                    return S[0] - E[0];  // 종료 시간이 같으면 시작 시간으로 정렬
+//                }
+//                return S[1] - E[1];  // 종료 시간 기준 정렬
+//            }
+//        });
+//
+//        int count = 0;
+//        int end = 0;
+//        for(int i = 0; i < N; i++) {
+//            if(A[i][0] >= end) {  // 시작 시간이 이전 종료 시간 이후면 회의실 사용 가능
+//                end = A[i][1];
+//                ++count;
+//            }
+//        }
+//        System.out.println(count);
+//    }
 }
---
+++ src/numberTheory/PrimeNumberFinder/FindPrimeNumbers.java
@@ -0,0 +1,35 @@
+package numberTheory.PrimeNumberFinder;
+
+import java.util.*;
+import java.io.*;
+
+public class FindPrimeNumbers {
+//    public static void main (String[] args) throws IOException {
+//
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(bf.readLine());
+//        int M = Integer.parseInt(st.nextToken());
+//        int N = Integer.parseInt(st.nextToken());
+//
+//        int[] A = new int[N+1];
+//        for(int i = 2; i <= N; i++) {
+//            A[i] = i;
+//        }
+//
+//        for(int i = 2; i <= Math.sqrt(N); i++) {
+//            if(A[i] == 0) {
+//                continue;
+//            }
+//
+//            for(int j = i+i; j <= N; j += i) {
+//                A[j] = 0;
+//            }
+//        }
+//
+//        for(int i = M; i <= N; i++) {
+//            if(A[i] != 0) {
+//                System.out.println(A[i]);
+//            }
+//        }
+//    }
+}
